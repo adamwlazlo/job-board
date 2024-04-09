4ee067fa7566f087ca87b64b8b8ccf42e686b0c6
Adam Wlazlo <adam.wlazlo@gmail.com>
2024-04-09 22:28:28 +0200

    jobs page creating using laravel components and styling using tailwind

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,5 +1,25 @@
 <x-layout>
     @foreach($jobs as $job)
-        <div class="mb-4 rounded-md border border-slate-300 bg-white p-4 shadow-sm">{{ $job->title }}</div>
+        <x-card class="mb-4">
+            <div class="mb-4 flex justify-between">
+                <h2 class="text-lg font-medium">{{ $job->title }}</h2>
+                <div class="text-slate-500">{{ number_format($job->salary, 2) }} PLN</div>
+            </div>
+
+            <div class="mb-4 flex justify-between text-sm text-slate-500 items-center">
+                <div class="flex space-x-4">
+                    <div class="class">Company name</div>
+                    <div class="class">{{ $job->location }}</div>
+                </div>
+                <div class="flex space-x-1 text-xs">
+                    <x-tag>{{ Str::ucfirst($job->experience) }}</x-tag>
+                    <x-tag>{{ $job->category }}</x-tag>
+                </div>
+            </div>
+
+            <p class="text-sm text-slate-500">
+                {!! nl2br(e($job->description)) !!}
+            </p>
+        </x-card>
     @endforeach
 </x-layout>
