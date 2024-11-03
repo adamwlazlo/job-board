49b24f06238b2a36507c2ef09e071b6c33efa32f
Adam Wlazlo <adam.wlazlo@gmail.com>
2024-11-03 21:37:50 +0100

    adding additional information about job applications and removing job application
    file loading form (changes in job_applications table, JobApplication model and controller, and in config file) and form error handling

--- resources/views/my_job_application/index.blade.php
+++ resources/views/my_job_application/index.blade.php
@@ -6,11 +6,37 @@
     @forelse($applications as $application)
 
         <x-job-card :job="$application->job" class="mb-4 text-sm">
+            <div class="flex items-center justify-between text-small text-slate-500">
+                <div>
+                    <div>
+                        Applied: {{$application->created_at->diffForHumans()}}
+                    </div>
+                    <div>
+                        Other {{Str::plural('applicant', $application->job->job_applications_count - 1)}}
+                        : {{$application->job->job_applications_count}}
+                    </div>
+                    <div>
+                        Your asking salary: ${{number_format($application->expected_salary)}}
+                    </div>
+                    <div>
+                        Average salary: ${{number_format($application->job->job_applications_avg_expected_salary)}}
+                    </div>
+                </div>
+                <div>
+                    <form action="{{route('my-job-applications.destroy', $application)}}" method="post">
+                        @csrf
+                        @method('delete')
+                        <x-button>Cancel</x-button>
+                    </form>
+                </div>
+            </div>
         </x-job-card>
     @empty
-        <x-card class="mb-4 text-sm">
-            No applications
-
-        </x-card>
+        <div class="rounded-md border border-dashed border-slate-300 p-8">
+            <div class="text-center font-medium">No applications</div>
+            <div class="text-center">Go find some jobs
+                <a class="text-indigo-500 hover:underline" href="{{route('jobs.index')}}">here!</a>
+            </div>
+        </div>
     @endforelse
 </x-layout>
