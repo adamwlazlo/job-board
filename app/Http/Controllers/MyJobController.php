696e666c632c9e80f3d5caf134451d66a240eba9
Adam Wlazlo <adam.wlazlo@gmail.com>
2025-01-08 21:40:13 +0100

    adding the ability to edit the offer

--- app/Http/Controllers/MyJobController.php
+++ app/Http/Controllers/MyJobController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\JobRequest;
 use App\Models\Job;
 use Illuminate\Http\Request;
 
@@ -35,18 +36,9 @@ class MyJobController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(JobRequest $request)
     {
-        $validatedDate = $request->validate([
-            'title' => 'required|string|max:255',
-            'location' => 'required|string|max:255',
-            'salary' => 'required|numeric|min:5500',
-            'description' => 'required|string',
-            'category' => 'required|in:' . implode(',', Job::$categories),
-//            'experience' => 'required|in:' . implode(',', Job::$experience),
-        ]);
-
-        auth()->user()->employer->jobs()->create($validatedDate);
+        auth()->user()->employer->jobs()->create($request->validated());
 
         return redirect()->route('my-jobs.index')
             ->with('success', 'Job position created successfully');
@@ -63,17 +55,20 @@ class MyJobController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Job $myJob)
     {
-        //
+        return view('my_jobs.edit', ['job' => $myJob]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(JobRequest $request, Job $myJob)
     {
-        //
+        $myJob->update($request->validated());
+
+        return redirect()->route('my-jobs.index')
+            ->with('success', 'Job updated successfully');
     }
 
     /**
--- resources/views/my_jobs/edit.blade.php
+++ resources/views/my_jobs/edit.blade.php
@@ -1,45 +1,46 @@
 <x-layout>
-    <x-breadcrumbs :links="['My jobs' => route('my-jobs.index'), 'Create' => '#']" class="mb-4"/>
+    <x-breadcrumbs :links="['My jobs' => route('my-jobs.index'), 'Edit Job' => '#']" class="mb-4"/>
 
     <x-card class="mb-4">
-        <form action="{{route('my-jobs.store')}}" method="post">
+        <form action="{{route('my-jobs.update', $job)}}" method="post">
             @csrf
+            @method('put')
 
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div class="col-span-2">
                     <x-label for="title" :required="true">Job title</x-label>
-                    <x-text-input name="title"/>
+                    <x-text-input name="title" :value="$job->title"/>
                 </div>
                 <div>
                     <x-label for="location" :required="true">Location</x-label>
-                    <x-text-input name="location"/>
+                    <x-text-input name="location" :value="$job->location"/>
                 </div>
                 <div>
                     <x-label for="salary" :required="true">Salary</x-label>
-                    <x-text-input name="salary" type="number"/>
+                    <x-text-input name="salary" type="number" :value="$job->salary"/>
                 </div>
 
                 <div class="col-span-2">
                     <x-label for="description" :required="true">Description</x-label>
-                    <x-text-input name="description" type="textarea" />
+                    <x-text-input name="description" type="textarea" :value="$job->description"/>
                 </div>
 
                 <div>
                     <x-label for="experience" :required="true">Experience</x-label>
-                    <x-radio-group name="experience" :value="old('experience')"
+                    <x-radio-group name="experience" :value="$job->experience"
                                    :all-options="false"
                                    :options="array_combine(array_map('ucfirst', \App\Models\Job::$experience), \App\Models\Job::$experience)"/>
                 </div>
 
                 <div>
                     <x-label for="category" :required="true">Categories</x-label>
-                    <x-radio-group name="category" :value="old('category')"
+                    <x-radio-group name="category" :value="$job->category"
                                    :all-options="false"
-                                   :options="\App\Models\Job::$categories" />
+                                   :options="\App\Models\Job::$categories"/>
                 </div>
 
                 <div class="col-span-2">
-                <x-button class="w-full">Create</x-button>
+                    <x-button class="w-full">Create</x-button>
                 </div>
             </div>
         </form>
